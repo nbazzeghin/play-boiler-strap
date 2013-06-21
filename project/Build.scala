70c2b8b7c71a1f7bbf8ffaf3a954fedc501e3ea9
Nigel Bazzeghin <nbazzeghin@gmail.com>
2013-06-21 14:51:18 -0500

    Initial Commit

---
+++ project/Build.scala
@@ -0,0 +1,29 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "playboilerstrap"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean
+  )
+
+	// Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory 
+	def customLessEntryPoints(base: File): PathFinder = ( 
+	    (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+	    (base / "app" / "assets" / "stylesheets" / "bootstrap" * "responsive.less") +++ 
+	    (base / "app" / "assets" / "stylesheets" * "*.less")
+	)
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints)      
+  )
+
+}
